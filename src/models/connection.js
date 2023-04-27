2adfa34d56d9cb25ad99d92e55d474368d8ac598
Jainefranciellem <jainefraciellen@gmail.com>
2023-04-26 21:04:16 -0300

    feat: endpoints created to list products

--- src/models/connection.js
+++ src/models/connection.js
@@ -4,8 +4,7 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  port: process.env.PORT,
-  database: process.env.MYSQL_DATABASE || 'store_manager_db',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
