024c4e4420aa7d7cdc109dac61a8ca48ebff8553
Jainefranciellem <jainefraciellen@gmail.com>
2023-04-27 21:59:46 -0300

    feat: Create validations products

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const productController = require('../controllers');
+const { validationName } = require('../middlewares/productValidations');
 
 router.get('/products', productController.getAll);
 router.get('/products/:id', productController.getById);
-router.post('/products', productController.create);
+router.post('/products', validationName, productController.create);
 
 module.exports = router;
